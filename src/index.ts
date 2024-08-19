8e6fe24ef3f5bddfbcdb66937aa287808d4fdb5f
viet12345 <vietdh42@wru.vn>
2024-08-19 16:25:33 +0700

    Optional Chaining

--- src/index.ts
+++ src/index.ts
@@ -5,24 +5,37 @@
 // }
 
 //interface
+interface Pet {
+    name: string
+}
+
 interface IStudent {
     name: string
     age: number
     email?: string
     isActive: boolean
+    pet?: Pet
 }
 
 const student: IStudent[] = []
 
 const newStudent_1: IStudent = {
-    name: '',
+    name: 'A',
     age: 0,
-    email: '1',
+    email: 'email@gmail.com',
     isActive: true,
 }
+student.push(newStudent_1)
+
 function GetEmail(studentEmail: string) {
     return studentEmail
 }
 if (newStudent_1.email) {
     console.log(GetEmail(newStudent_1.email))
 }
+//Optional Chaining => mục đích là không để giá trị trả ra là unđefined
+function GetPetName(student: IStudent) {
+    return student.pet?.name || ''
+}
+
+console.log(GetPetName(newStudent_1))
