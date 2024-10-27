38abb4d13440e03c7697aaf51dd05b7d3254b15c
DESKTOP-8PAFO7M\AS-L34 <vietdh42@wru.vn>
2024-10-27 21:26:49 +0700

    update report allure

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'cypress'
+import { allureCypress } from 'allure-cypress/reporter'
 
 export default defineConfig({
     e2e: {
+        setupNodeEvents(on, config) {
+            allureCypress(on, config, {
+                resultsDir: 'allure-results',
+            })
+            return config
+        },
+        screenshotOnRunFailure: true,
         baseUrl: 'https://example.cypress.io',
     },
 })
